0af44714e246941c7cd348ebab059b6f3b9596b0
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-07-24 10:11:55 +0600

    GitHub provider added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
+  const { data } = useSession()
+
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
+      {data?.user && <h3 style={{ textAlign: "center", marginTop: "1%" }}>LoggedIn user: {data.user.email}</h3>}
     </div>
   );
 };
